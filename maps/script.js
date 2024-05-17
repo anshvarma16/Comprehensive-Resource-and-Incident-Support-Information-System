279038affb8c245874bc4fc37359311fd0a12d3a
anshvarma16 <87943893+anshvarma16@users.noreply.github.com>
2024-05-17 11:33:00 +0530

    Add files via upload

---
+++ maps/script.js
@@ -0,0 +1,42 @@
+function initMap() {
+	// Update MAP_ID with custom map ID
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+            lat: 28.5938,
+            lng: 77.0435,
+		},
+		zoom: 10,
+		mapId: '67bf9a15371336d2',
+		mapTypeControl: false,
+		fullscreenControl: false,
+		streetViewControl: false,
+	});
+
+	// Name
+	// Latitude, Longitude
+	// Image URL
+	// scaledSize width, height
+
+	for (let i = 0; i < markers.length; i++) {
+		const currMarker = markers[i];
+
+		const marker = new google.maps.Marker({
+			position: { lat: currMarker[1], lng: currMarker[2] },
+			map,
+			title: currMarker[0],
+			icon: {
+				url: currMarker[3],
+				scaledSize: new google.maps.Size(currMarker[4], currMarker[5]),
+			},
+			animation: google.maps.Animation.DROP,
+		});
+
+		const infowindow = new google.maps.InfoWindow({
+			content: currMarker[0],
+		});
+
+		marker.addListener('click', () => {
+			infowindow.open(map, marker);
+		});
+	}
+}
